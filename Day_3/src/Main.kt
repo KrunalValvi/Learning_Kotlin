0fbd6faaa5edd205e28dbddeaf4eb1b251eaace1
Krunal <valvikrunal69@gmail.com>
2024-12-17 21:41:19 +0530

    second2

---
+++ Day_3/src/Main.kt
@@ -0,0 +1,11 @@
+
+fun main(){
+
+    val x = 3
+    val y = 4
+    val z = 5
+
+    println("Your result of x is: ${x}")
+    println("Your result of x + y is: ${x + y}")
+    println("Your result of x + y + z is: ${x + y + z}")
+}
