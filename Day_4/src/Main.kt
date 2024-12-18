aa4775734e683d42ab133386ac0d63d4b22a70c7
Krunal <valvikrunal69@gmail.com>
2024-12-18 10:46:59 +0530

    second4

--- Day_4/src/Main.kt
+++ Day_4/src/Main.kt
@@ -1,4 +1,19 @@
+class Person {
+    var name: String = "Unknown"
+    var age: Int = 0
+
+    // Method
+    fun introduce() {
+        println("Hi, my name is $name and I am $age years old.")
+    }
+}
 
 fun main(){
 
+    var person = Person()
+
+    person.name = "Krunal"
+    person.age = 19
+
+    person.introduce()
 }
