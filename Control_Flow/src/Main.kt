5845453722286b46069cfc3bf8620ef610846ca3
Krunal <valvikrunal69@gmail.com>
2024-12-18 12:49:13 +0530

    second4

---
+++ Control_Flow/src/Main.kt
@@ -0,0 +1,57 @@
+fun main(){
+//
+////    If Else
+//
+//    val number = 12
+//
+//    if (number % 2 ==0){
+//        println("$number is even")
+//    }else{
+//        println("$number is odd")
+//    }
+//
+//
+////    When
+//
+//    val grade = '4'
+//
+//    when(grade){
+//        'A'-> println("Excellent")
+//        'B'-> println("Good")
+//        else-> println("Need Improvement")
+//    }
+//
+////    For Loop
+//
+//    for (i in 1..5){
+//        println(i)
+//    }
+//
+////    While Loop
+//
+//    var count = 5
+//    while (count>0){
+//        println("Countdown: $count")
+//        count--
+//    }
+
+    var number = -2
+
+    if (number>0){
+        println("Number is positive")
+    }else if (number<0){
+        println("Number is negative")
+    }else{
+        println("Number is Zero")
+    }
+
+
+    for (i in 1..10){
+        if (i % 2 ==0){
+            println("$i is Even")
+        }else{
+            println("$i is odd")
+        }
+    }
+
+}
---
+++ Day_5/src/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+
+    val car = car()
+    car.brand = "Porsche"
+    car.model = "911"
+    car.year = 1963
+
+    car.start()
+    car.display()
+    car.stop()
+
+    println("")
+
+   val dog = dog()
+    dog.name = "tommy"
+
+    dog.eat()
+    dog.sleep()
+    dog.bark()
+
+
+}
